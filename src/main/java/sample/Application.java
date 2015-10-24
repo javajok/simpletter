9ebfbad938eb4efc2729ca82c1c58285bba91fbf
irof <irof@hogedriven.net>
2015-10-24 21:23:37 +0900

    エラーページ足してみる

--- src/main/java/sample/Application.java
+++ src/main/java/sample/Application.java
@@ -1,7 +1,12 @@
 package sample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author irof
@@ -10,5 +15,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    @Bean
+    public HandlerExceptionResolver handler() {
+        return (request, response, handler, ex) -> {
+            logger.error("なんかよくわからないけどとにかく例外が発生したー", ex);
+            ModelAndView mav = new ModelAndView();
+            mav.addObject("exception", ex);
+            mav.setViewName("error");
+            return mav;
+        };
     }
 }
