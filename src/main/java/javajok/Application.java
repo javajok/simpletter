6388fdcf96360e64fcc25104e093b92cbc615ad2
irof <irof@hogedriven.net>
2015-10-25 17:09:27 +0900

    indexを置いてみた

--- src/main/java/javajok/Application.java
+++ src/main/java/javajok/Application.java
@@ -2,13 +2,21 @@ package javajok;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author irof
  */
+@Controller
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
     }
 }
