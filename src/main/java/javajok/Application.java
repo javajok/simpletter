f256d9490b6ec24df7d824f69bb464c93785495e
irof <irof@hogedriven.net>
2015-11-04 07:53:57 +0900

    Applicationにコメントを記述

--- src/main/java/javajok/Application.java
+++ src/main/java/javajok/Application.java
@@ -6,6 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
+ * アプリケーションのエントリポイントとなるクラスです。
+ * 動作を確認したい場合は、IDEからこのクラスを実行してください。
+ *
+ * このクラスの実装はSpringBoot固有であり、書き方はほぼ固定になります。
+ * そのため、このクラスは *ワークショップ対象外* です。とりあえずは気にしないでください。
+ *
  * @author irof
  */
 @Controller
@@ -15,6 +21,11 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    /**
+     * http://localhost:8080 にアクセスした際に動作するコントローラーです。
+     * このメソッドを動作させるために Application クラスに @Controller を付与していますが、
+     * 無くてもSpringBootは動作します。
+     */
     @RequestMapping("/")
     public String index() {
         return "index";
