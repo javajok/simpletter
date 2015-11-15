968547be5b71d602923e4f1076bc462fe165f9a0
irof <irof@hogedriven.net>
2015-11-15 12:20:55 +0900

    複数件を伝えるためにListを返してeachで回す

--- src/main/java/javajok/work/TimelineController.java
+++ src/main/java/javajok/work/TimelineController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+
 @Controller
 public class TimelineController {
 
@@ -14,7 +16,15 @@ public class TimelineController {
         tweet.userId = "hogehoge";
         tweet.text = "練習用だよ";
 
-        model.addAttribute("tweet", tweet);
+        Tweet tweet2 = new Tweet();
+        tweet2.userId = "fugafuga";
+        tweet2.text = "練習用2";
+
+        ArrayList<Object> timeline = new ArrayList<>();
+        timeline.add(tweet);
+        timeline.add(tweet2);
+
+        model.addAttribute("timeline", timeline);
 
         return "timeline";
     }
