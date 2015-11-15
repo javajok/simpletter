50984fb66d8cc52ee6a0e19ffc3531b14eef0933
irof <irof@hogedriven.net>
2015-11-15 12:10:59 +0900

    ツイートを表示してみます

--- src/main/java/javajok/work/TimelineController.java
+++ src/main/java/javajok/work/TimelineController.java
@@ -1,5 +1,6 @@
 package javajok.work;
 
+import javajok.sample.Tweet;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,12 @@ public class TimelineController {
 
     @RequestMapping("/timeline")
     public String timeline(Model model) {
+        Tweet tweet = new Tweet();
+        tweet.userId = "hogehoge";
+        tweet.text = "練習用だよ";
+
+        model.addAttribute("tweet", tweet);
+
         return "timeline";
     }
 }
