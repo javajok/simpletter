8a94920b0173c81abfd260347ac24dc28ddd09b4
irof <irof@hogedriven.net>
2015-11-15 11:51:16 +0900

    /hello にアクセスされた時に動くようにSpringに教えます

--- src/main/java/javajok/work/HelloController.java
+++ src/main/java/javajok/work/HelloController.java
@@ -1,10 +1,12 @@
 package javajok.work;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 
+    @RequestMapping("/hello")
     public String hello() {
         return "hello";
     }
