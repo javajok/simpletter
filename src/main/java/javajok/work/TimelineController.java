b73ac82ff213efdf8a305489b20024ae231e0d14
irof <irof@hogedriven.net>
2015-11-15 12:43:10 +0900

    受け取ったメッセージをAPIに伝えるようにします

--- src/main/java/javajok/work/TimelineController.java
+++ src/main/java/javajok/work/TimelineController.java
@@ -4,7 +4,10 @@ import javajok.sample.Timeline;
 import javajok.sample.Tweet;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -23,7 +26,15 @@ public class TimelineController {
     }
 
     @RequestMapping("/tweet")
-    public String tweet() {
+    public String tweet(@RequestParam String text) {
+        LinkedMultiValueMap<Object, Object> param = new LinkedMultiValueMap<>();
+        param.add("userId", "hogekun");
+        param.add("text", text);
+
+        new RestTemplate().postForEntity("http://localhost:8090/tweet",
+                param,
+                Tweet.class);
+
         return "redirect:/timeline";
     }
 }
